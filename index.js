ace04d5d1a7f0f42eeca724a2235fac32e92cfa2
Ridwan4D <ridwanul161@gmail.com>
2024-08-18 15:20:22 +0600

    post method for upload user data

--- index.js
+++ index.js
@@ -28,7 +28,19 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        
+        const userCollection = client.db("rtProductPreview").collection("users");
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { userEmail: user.userEmail }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: "user already exist", insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
 
 
 
